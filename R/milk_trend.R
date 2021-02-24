9e0d87cea503d0135d06ab43f4d418c9457d60c2
quin-smith <quinlan_smith@yahoo.com>
2021-02-23 18:43:46 -0800

    changed r milk_trend.R to get it to work

--- R/milk_trend.R
+++ R/milk_trend.R
@@ -10,5 +10,7 @@ milk_trend <- read.csv(here("data","milk.csv")) %>%
   clean_names() %>% 
   mutate(value = str_replace_all(value, pattern = ",", replacement = "")) %>%
   mutate(milk_kg = as.numeric(value) * 0.4535924) %>% 
-  rename(milk_lb = value) %>% 
+  rename(milk_lb = value)
+
+milk_trend <- milk_trend %>% 
   mutate(state = str_to_title(milk_trend$state))
