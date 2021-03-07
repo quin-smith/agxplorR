30d4d007703ea77f39bc2e1e13538eb3bb2e9bf6
quin-smith <quinlan_smith@yahoo.com>
2021-03-06 17:04:35 -0800

    added serv_impact dataframe and R script

--- R/serv_impact.R
+++ R/serv_impact.R
@@ -5,5 +5,9 @@ library(readxl)
 
 serv_impact <- read_excel(here("data", "pnas.1906908116.sd01.xlsx")) %>% 
   clean_names() %>% 
-  select(food_group, ghg_emissions_rank, land_use_rank, eutrophication_potential_ep_rank, acidification_potential_ap_rank, scarcity_weighted_water_use_rank)
+  select(food_group, ghg_emissions_rank, land_use_rank, 
+         eutrophication_potential_ep_rank, acidification_potential_ap_rank, 
+         scarcity_weighted_water_use_rank) %>% 
+  column_to_rownames(var = "food_group")
 
+serv_impact <- rbind(rep(15,6), rep(1,6), serv_impact)
